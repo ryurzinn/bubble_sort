85d5935f30b93894e064b3f63bba8c906dd42490
ryurzinn <chinwenwencha47@gmail.com>
2025-09-02 09:26:06 -0300

    bubble sort algorithm python - facultad

---
+++ bubble-sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(len(numbers)):
+    "numeros a ingresar"
+    for i in range (len(numbers))
+        for j in range(len(numbers) - 1)
+            if (numbers[j] > numbers[j + 1])
+                numbers[j], numbers[j + 1] = numbers[j + 1], numbers [j]
+                    print(f"intercambio de numeros {numbers}")
+
+lista_numeros = [23, 546 ,754 ,32 ,1, 6,3]
+print(f"lista de numeros es {lista_numeros}")
+
+bubble_sort(lista_numeros)
+
+print(f"la lista de numeros ordenados es: {lista_numeros}")
